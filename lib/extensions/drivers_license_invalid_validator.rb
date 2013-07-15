c13d89557727bbd15571034dd6d6c21f69dc4f3d
Andrew Medeiros <amedeiros0920@gmail.com>
2013-07-15 13:35:12 -0400

    Change from .nil? to .blank?

--- lib/extensions/drivers_license_invalid_validator.rb
+++ lib/extensions/drivers_license_invalid_validator.rb
@@ -1,17 +1,16 @@
-class DriversLicenseInvalidValidator < ActiveModel::Validator
+class DriversLicenseInvalidValidator
   def validate(record)
     if !@options[:attributes].include?(:drivers_license_number)
-      record.errors.add(:base, 'Missing required supplied attribute drivers_license_number')
+      record.errors.add(:base, 'Missing required attribute drivers_license_number')
     elsif !@options[:attributes].include?(:drivers_license_state)
-      record.errors.add(:base, 'Missing required supplied attribute drivers_license_state')
-    elsif record.drivers_license_number.nil?
-      record.errors.add(:drivers_license_number, 'Attribute drivers_license_number cannot be blank.')
-    elsif record.drivers_license_state.nil?
-      record.errors.add(:drivers_license_state, 'Attribute drivers_license_state cannot be blank.')
+      record.errors.add(:base, 'Missing required attribute drivers_license_state')
+    elsif record.drivers_license_number.blank?
+      record.errors.add(:drivers_license_number, 'cannot be blank.')
+    elsif record.drivers_license_state.blank?
+      record.errors.add(:drivers_license_state, 'cannot be blank.')
     else
       invalid = DlValidator.invalid?(record.drivers_license_number, record.drivers_license_state)
       record.errors.add(:base, 'Drivers license is invalid.') if invalid
     end
   end
 end
-
