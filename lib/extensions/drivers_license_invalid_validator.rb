712d2018f849fc7160c2bb29bfaec447d33ac01b
Andrew Medeiros <amedeiros0920@gmail.com>
2013-07-15 11:08:36 -0400

    More changes to validation

--- lib/extensions/drivers_license_invalid_validator.rb
+++ lib/extensions/drivers_license_invalid_validator.rb
@@ -1,12 +1,13 @@
 class DriversLicenseInvalidValidator < ActiveModel::Validator
   def validate(record)
-    if record.drivers_license_number.nil?
-      record.errors.add(:base, 'Missing attribute drivers_license_number')
-    elsif record.drivers_license_state.nil?
-      record.errors.add(:base, 'Missing attribute drivers_license_state')
+    if !record.attributes.has_key?('drivers_license_number') or record.drivers_license_number.nil?
+      record.errors.add(:base, 'Missing or nil attribute drivers_license_number')
+    elsif !record.attributes.has_key?('drivers_license_state') or record.drivers_license_state.nil?
+      record.errors.add(:base, 'Missing or nil attribute drivers_license_state')
     else
       invalid = DlValidator.invalid?(record.drivers_license_number, record.drivers_license_state)
       record.errors.add(:base, 'Drivers license is invalid.') if invalid
     end
   end
 end
+
